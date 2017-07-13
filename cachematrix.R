eb1ba4a49e81870cc5caaea2212113c76bef423c
Donal-Lynch <lynchdo@tcd.ie>
2017-07-14 10:11:34 +1200

    matrix inversion code added

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function creates a matrix object with 4 associated
+## functions;
+## set ()       Sets the matrix data
+## get ()       Returns the matrix data
+## setInv ()    Sets the inverse of the matrix
+## getInv ()    Return the inverse of the matrix
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    
+    inv_cached <- NULL
+    set <- function(y) {
+        x <<- y
+        inv_cached <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inv_set) inv_cached <<- inv_set
+    getinv <- function() inv_cached
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
+    
+    
 }
 
+## Return a matrix that is the inverse of 'x'
+## This function takes a 'makeCacheMatrix' object,
+## it then checks to see if the inverse has been created already
+## and either retruns the cached matrix or calculats and returns the
+## matrix
 
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+    inv_cached <- x$getinv()
+    if(!is.null(inv_cached)) {
+        message("getting cached data")
+        return(inv_cached)
+    }
+    data <- x$get()
+    inv_cached <- solve (data, ...)
+    x$setinv(inv_cached)
+    inv_cached
+    
 }
